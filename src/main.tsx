4dc329e1015fc3468c6ee03fe7a0b9e2eda3fcd5
ManuLosta <mlostalom@gmail.com>
2024-03-20 22:17:02 -0300

    add initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './pages/Home.tsx';
+import { NextUIProvider } from '@nextui-org/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <Home />
+    </NextUIProvider>
   </React.StrictMode>,
-)
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@nextui-org/react';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1 className="font-bold text-3xl">Home</h1>
+      <Button color="primary">Hola</Button>
+    </div>
+  );
+}
