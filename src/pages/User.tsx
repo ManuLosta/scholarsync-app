2c53d0fd759068a168b79631f6e89ba522a1043d
ManuLosta <mlostalom@gmail.com>
2024-04-12 14:36:10 -0300

    add simple user page

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,37 +1,39 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import api from '../api.ts';
+import { Button } from '@nextui-org/react';
+
+type User = {
+  id: string
+  firstName: string;
+  lastName: string;
+  username: string;
+}
 
 export default function User() {
-  // extender de algo
-  // Clase que este encargada de hacer todos los fetsh
-  // User service que haga fechs al backend
   const { id } = useParams();
-  // Debuelve el objeto pero usamos el id, que se localiza con las llaves
-  const [data, setData] = useState();
+  const [user, setUser] = useState<User>();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    let path = `http://localhost:8080`;
+    api.get(`users/${id}`)
+      .then(res => {
+        const user: User = res.data;
+        setUser(user);
+      }).finally(() => setLoading(false))
 
-    // Le pasas funciones y dependencias
-
-    // Logica de hacer la request al servidor y que te retorne un userId.
-    const fetchData = async () => {
-      // Debe ser asincronica para que no se detenga el resto del programa
-
-      const response = await fetch(`${path}/api/v1/users/${id}`); // Se espera a que se complete
-
-      if (!response.ok) {
-        //TODO respuesta no esta bien
-      }
-
-      const responseData = await response.json();
-      setData(responseData);
-      console.log(data);
-    };
-
-    fetchData();
-    //  Detecta cambios en el use-efect
   }, [id]);
 
-  return <div>{data.email}</div>;
+ return loading ? (
+   <div>
+     Loading
+   </div>
+ ) : (
+    <div>
+      <h1>{user?.firstName} {user?.lastName}</h1>
+      <p>{user?.username}</p>
+      <Button>Agregar amigo</Button>
+      <Button>Invitar a grupo</Button>
+    </div>
+ )
 }
