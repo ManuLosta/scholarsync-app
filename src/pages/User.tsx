bda76e8971f6baf85eb053008b9db5a6d88ff32e
ManuLosta <mlostalom@gmail.com>
2024-04-18 23:43:01 -0300

    refactor userpage component

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,14 +1,13 @@
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../api.ts';
 import { Button } from '@nextui-org/react';
 import { useAuth } from '../hooks/useAuth.ts';
-import { object } from 'zod';
-
+import ExampleComponent from '../components/ExampleComponent.tsx';
 
 type receivedFriendRequests = {
-  from_id: String
-  to_id: String
+  from_id: string
+  to_id: string
 }
 
 interface Friend {
@@ -19,68 +18,49 @@ interface Group {
   [key: string]: any;
 }
 
-
 type User = {
-  firstName: String;
-  lastName: String;
-  userName: String;
-  credits: Number;
-  email: String;
-  birthDate: String;
-  createdAt: String;
-  id: String;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  credits: number;
+  email: string;
+  birthDate: string;
+  createdAt: string;
+  id: string;
   receivedFriendRequests: receivedFriendRequests[];
   friends: Friend[];
   groups: Group[];
 }
 
-
-
-
-
-function DeleteFriends(user: User | undefined, currentId: String | undefined){}
-
-
-
-
-
 export default function User() {
   const { id } = useParams();
   const [user, setUser] = useState<User>();
   const [loading, setLoading] = useState(true);
-  const [textForButton, settextForButton] = useState("Agregar amigo");
   const auth = useAuth();
   const currentId = auth?.user?.id;
-  const [requestState, setRequestState] = useState<String>();  
+  const [requestState, setRequestState] = useState<string>("");
 
   const cases = [
-    "user/not-found", "friend-request/person-already-send-us-friend-request", "friend-request/already-sent", "user/already-a-friend", "user/can-send-request"
-  ]
+    'user/not-found', 'friend-request/person-already-send-us-friend-request', 'friend-request/already-sent', 'user/already-a-friend', 'user/can-send-request',
+  ];
 
 
-  function sendFriendRequest(){
-  
-    if(user == undefined || currentId == undefined){
-      return;
-    }
-  
+  function sendFriendRequest() {
     const data = {
       from_id: currentId,
-      to_id: user?.id
-    }
-    
+      to_id: user?.id,
+    };
+
     api
-    .post("friend-requests/send-friend-request", data)
-    .then(() => {
-  
-      settextForButton("Pendiente")
-  
-    })
-  
+      .post('friend-requests/send-friend-request', data)
+      .then(() => {
+        setRequestState(cases[2])
+      })
+      .catch(err => console.error(err));
   }
 
 
-  function makerequesState(){
+  function makerequesState() {
 
     // Estados: 
     // Son amigos
@@ -89,138 +69,92 @@ export default function User() {
     // no son amigos
 
     // Son amigos:
-    
-    if (user?.friends.some(friend => friend.id === currentId)){
-      setRequestState(cases[3])
-      return
+
+    if (user?.friends.some(friend => friend.id === currentId)) {
+      setRequestState(cases[3]);
+      return;
     }
+
     // El me envio una request a mi
-    
-    let data;
-
     api.get(`friend-requests/${currentId}/friend-requests`)
-    .then(res => {
-      data = res.data
-      for (const objeto of data){
-        if (objeto.to == currentId){
-          setRequestState(cases[1])
-          console.log("aceptar solicitud")
-          return
+      .then(res => {
+        const requests = res.data;
+        for (const request of requests) {
+          if (request.from == user?.id) {
+            setRequestState(cases[1]);
+            break;
+          }
         }
-      }
-      
-    })
-    .catch((err)=>{console.log(err)} )
-
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
     // la request esta pendiente
-
     api.get(`friend-requests/${id}/friend-requests`)
-    .then(res => {
-      data = res.data
-
-      for (const objeto of data){
-        if (objeto.from == currentId){
-          console.log("pendiente")
-          setRequestState(cases[2])
-          return
-
+      .then(res => {
+        const data = res.data;
+        for (const objeto of data) {
+          if (objeto.from == currentId) {
+            setRequestState(cases[2]);
+            break;
+          }
         }
-      }
-      
-    })
-    .catch((err)=>{console.log(err)} )
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-
-
-    setRequestState(cases[4])
-    console.log(" no pendiente")
-
-    
-
+    setRequestState(cases[4]);
   }
 
-
-
-
-  function setUpState(){
-
-   
-    if ((user != undefined) && (currentId != undefined) && (requestState != undefined)){
-    
-      
-  
-       switch(requestState){
-        case cases[0]:
-          console.log("user-not-foud")
-          break;
-        case cases[1]:
-          settextForButton("aceptar-Solicitud")
-          break;
-        case cases[2]:
-          settextForButton("Pendiente")
-        break;
-        case cases[3]:
-          settextForButton("quitar amigo")
-          break;
-        default:
-          settextForButton("agregar amigo")
-       }
-  
-    }
-    
-  
-  } 
-
-  function buntonEngine(){
-    switch(requestState){
-
+  function buttonEngine() {
+    switch (requestState) {
       case cases[4]:
-        sendFriendRequest()
+        sendFriendRequest();
         break;
-
       case cases[3]:
         // Eliminar amigo Implementar en el futuro
         break;
       case cases[2]:
         // Pendiente, no hace nada, 
         // Quizas en el futuro, eliminar solicitud
-      break;
+        break;
       case cases[1]:
-
-       
-
-      break;
-
+        break;
     }
-  
-    makerequesState()
-    setUpState()
-  
-  
+    makerequesState();
   }
+
+  function getButtonText(id: string): string {
+    switch (id) {
+      case "friend-request/already-sent":
+        return "Pendiente"
+      case "friend-request/person-already-send-us-friend-request":
+        return "Aceptar solicitud"
+      case "user/already-a-friend":
+        return "Quitar amigo"
+      default:
+        return "Agregar amigo"
+    }
+  }
+
+  useEffect(() => {
+    api.get(`users/profile/${id}`)
+      .then(res => {
+        setUser(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [id]);
 
 
   useEffect(() => {
-    
-      api.get(`users/profile/${id}`)
-      .then(res => {
-        
+    makerequesState()
+    setLoading(false)
+  }, [user]);
 
-        setUser(res.data)
-
-        makerequesState() 
-        setUpState()
-      })
-      .catch((err)=>{console.log(err)} )
-      .finally(()=>{
-        setLoading(false)
-      })
-
-
-  }, [id]);
-
-  
 
   return loading ? (
 
@@ -228,37 +162,23 @@ export default function User() {
       Loading
     </div>
   ) : (
-    
-    <div className='flex gap-5 flex-col'>
-     
-      
-    <h1>{user?.firstName} {user?.lastName}</h1>
-      
-    
+    <div className="flex gap-5 flex-col">
+      <h1>{user?.firstName} {user?.lastName}</h1>
       <p>{user?.userName}</p>
-      
-      
-
       <div>
-
-      <p>Puntos: {user?.credits?.toString()}</p>
-      
-      <p>Amigos: {user?.friends?.length?.toString()}</p>
-    
+        <p>Puntos: {user?.credits?.toString()}</p>
+        <p>Amigos: {user?.friends?.length?.toString()}</p>
       </div>
-      
       <div>
         <h3>
           Grupos:
-                  {user?.groups.map(group => (
-                    <Button key={group.id}>{group.name}</Button>
-                ))}
+          {user?.groups.map(group => (
+            <Button key={group.id}>{group.name}</Button>
+          ))}
         </h3>
-
       </div>
-      <Button onClick={() => buntonEngine()} color='primary'>{textForButton}</Button>
-      <Button color='secondary' >Invitar a grupo</Button>
-      
+      <Button onClick={() => buttonEngine()} color="primary">{getButtonText(requestState)}</Button>
+      <ExampleComponent userId={user?.id} myId={currentId} />
     </div>
-  )
+  );
 }
