a49460494dc2780a27d35ad2ea38d6c928a4cf4a
ManuLosta <mlostalom@gmail.com>
2024-04-19 10:23:34 -0300

    simple user page

--- src/pages/Group.tsx
+++ src/pages/Group.tsx
@@ -1,9 +1,17 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import api from '../api.ts';
+import { Avatar, Skeleton } from '@nextui-org/react';
 
 type Group = {
-  title: string
+  title: string;
+  description: string;
+  users: {
+    id: string;
+    username: string;
+    firstName: string;
+    lastName: string;
+  }[];
 }
 
 export default function Group() {
@@ -15,6 +23,7 @@ export default function Group() {
     api.get(`groups/getGroup?group_id=${groupId}`)
       .then(res => {
         const data = res.data;
+        console.log(data)
         setGroup(data);
       })
       .catch(err => {
@@ -23,10 +32,29 @@ export default function Group() {
       .finally(() => setLoading(false))
   }, [groupId]);
 
-  return group &&  (
-    <div className="container">
-      Group page
-      {groupId}
+  return loading ? (
+    <div className="container mt-8">
+      <div className="flex gap-4 items-center">
+        <Skeleton className="w-[80px] h-[80px] rounded-full" />
+        <div>
+          <Skeleton className="h-6 w-[200px] rounded-lg mb-2" />
+          <Skeleton className="h-4 w-[200px] rounded-lg mb-2"  />
+          <Skeleton className="h-3 w-[200px] rounded-lg" />
+        </div>
+      </div>
+    </div>
+    ) : (
+    <div className="container mt-8">
+      <div className="flex gap-4 items-center">
+        <Avatar className="w-[90px] h-[90px] text-2xl" name={group?.title} color="primary" />
+        <div>
+          <h1 className="font-bold text-3xl">{group?.title}</h1>
+          <p className="text-xl font-light">{group?.description}</p>
+          <p className="font-light">{group?.users?.length} {
+            (group?.users?.length != undefined && group?.users?.length > 1) ? "miembros" : "miembro"
+          }</p>
+        </div>
+      </div>
     </div>
   )
 }
