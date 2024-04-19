26298a89b1a0fd36ade1c74e3e4bf18724ffc79d
carla <cboggio@mail.austral.edu.ar>
2024-04-19 15:15:57 -0300

    add styles to profile

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../api.ts';
-import { CircularProgress } from '@nextui-org/react';
-import { User } from '@nextui-org/react';
+import { Button, CircularProgress } from '@nextui-org/react';
+
 import { useAuth } from '../hooks/useAuth.ts';
 import FriendStatusButton from '../components/FriendStatusButton.tsx';
+import {Avatar} from "@nextui-org/react";
 
 type receivedFriendRequests = {
   from_id: string
@@ -67,28 +68,37 @@ export default function UserProfile() {
     </div>
   ) : (
     <div className="flex gap-8 flex-col justify-center align-center ml-20 mt-9">
-      <div>
-      <User
+      <div className="flex gap-8 align-center">
+      <Avatar
           
-          name={`${UserProfile?.firstName} ${UserProfile?.lastName}`}
-          description={`@${UserProfile?.username}`}
+          name={`${UserProfile?.firstName}`}
+          className="w-20 h-20 text-large"
+          //description={`@${UserProfile?.username}`}
         />
+        <div className="flex gap-4 flex-col">
+          <p className='text-2xl'>${`${UserProfile?.firstName}     ${UserProfile?.lastName}`}</p>
+          <p className='text-xl text-foreground-400' >{`@${UserProfile?.username}`} </p>
+        </div>
       </div>
 
 
       <div className="flex gap-9 align-center">
         
-        <p  className="text-2xl" >Puntos:  {UserProfile?.credits?.toString()}</p>
-        <p className="text-2xl">Amigos:  {UserProfile?.friends?.length?.toString()}</p>
+        <p  className="text-xl" >Puntos:  {UserProfile?.credits?.toString()}</p>
+        <p className="text-xl">Amigos:  {UserProfile?.friends?.length?.toString()}</p>
       </div>
       <div>
-        <p className="text-2xl">
+        <p className="text-xl">
           Grupos:
 
         </p>
       </div>
-      
+      <div className="flex gap-4">
       <FriendStatusButton userId={UserProfile?.id} myId={currentId} />
+      <Button color="secondary">Invitar a grupo</Button>
+      
+      </div>
+
     </div>
   );
 }
