9ae14d2d973d6155cfeafd3c52c0c765fe0a60bb
carla <cboggio@mail.austral.edu.ar>
2024-05-26 19:02:46 -0300

    add profile pic to the user in the sideBar

--- src/components/auth/UserDropdown.tsx
+++ src/components/auth/UserDropdown.tsx
@@ -7,9 +7,34 @@ import {
 } from '@nextui-org/react';
 import { useAuth } from '../../hooks/useAuth.ts';
 import ThemeSwitcher from '../navigation/ThemeSwitcher.tsx';
+import api from '../../api.ts';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function UserDropdown() {
   const auth = useAuth();
+  const [profilePicture, setProfilePicture] = useState('');
+
+  const getImg = useCallback(async () => {
+    try {
+      const response = await api.get(`/users/get-profile-picture`, {
+        params: { user_id: auth?.user?.id },
+      });
+
+      const imageSrc = `data:image/png;base64,${response.data}`;
+      return imageSrc;
+    } catch (error) {
+      console.error('Error fetching profile picture:', error);
+    }
+  }, [auth?.user?.id]);
+
+  useEffect(() => {
+    const fetchProfilePicture = async () => {
+      const imgSrc = await getImg();
+      setProfilePicture(imgSrc || '');
+    };
+
+    fetchProfilePicture();
+  }, [getImg]);
 
   return (
     <Dropdown closeOnSelect={false} placement="bottom">
@@ -17,6 +42,9 @@ export default function UserDropdown() {
         <User
           name={`${auth?.user?.firstName} ${auth?.user?.lastName}`}
           description={`@${auth?.user?.username}`}
+          avatarProps={{
+            src: profilePicture,
+          }}
         />
       </DropdownTrigger>
       <DropdownMenu aria-label="Static Actions">
